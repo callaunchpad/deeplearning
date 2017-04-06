4d1e3bacb54cfe0091a482fe49e0ae423614ba20
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-04-06 02:41:28 -0700

    Added feature menu

--- app/scripts/webcam.js
+++ app/scripts/webcam.js
@@ -5,6 +5,7 @@ $(function() {
   var width = 0;
   var height = 0;
   
+  /*** BEGIN WEBCAM ***/
   navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
   
   navigator.getMedia({ video: true, audio: false }, function(stream) {
@@ -20,6 +21,7 @@ $(function() {
       console.error(error);
   });
 
+  /*** GET WIDTH AND HEIGHT ***/
   video.addEventListener('canplay', function(ev){
     if (!streaming) {
       width = $('video').width();
@@ -33,38 +35,60 @@ $(function() {
     }
   }, false);
 
+  /*** ADD EVENT LISTENER TO BUTTON ***/
   var status = 'live';
+  var img = false;
   $('#action-button').click(function(e) {
+    /*** ON LIVE, GET IMG, PROCESS IMG, RENDER RESULTS ***/
     if (status == 'live') {
       $('.overlay').css('display', 'block');
 
-      /* TODO: Make HTTP request */
+      // Draw image on canvas
+      var context = canvas.getContext('2d');
+      if (width && height) {
+        canvas.width = width;
+        canvas.height = height;
+        context.drawImage(video, 0, 0, width, height);
+      }
 
-      // Simluate HTTP request to Lambda
+      // Format image
+      img = canvas.toDataURL('image/png');
+
+      // Hide video
+      $('video').css('display', 'none');
+      $('canvas').css('display', 'block');
+
+
+
+      /* TODO: Make HTTP request */
+      // Object Detection (Single)
+      if (mode == 'f1') {
+
+      } 
+
+      // Object Detection (Real-time)
+      else if (mode == 'f2') {
+
+      }
+
+      // Face Recognition
+      else {
+
+      }
+
+
+
+      // Simluate HTTP request
       setTimeout(function() {
         status = 'result';
         
-        // Display overlay
+        // Hide overlay
         $('.overlay').css('display', 'none');
 
         // Change action button text
         $('#action-button').text('Reset');
-
-        // Hide video
-        $('video').css('display', 'none');
-
-        // Draw image on canvas
-        var context = canvas.getContext('2d');
-        if (width && height) {
-          console.log('Capture image!');
-          canvas.width = width;
-          canvas.height = height;
-          context.drawImage(video, 0, 0, width, height);
-        }
-
-        // Show canvas
-        $('canvas').css('display', 'block');
       }, 2000);
+    /*** ON LIVE, GET IMG ***/
     } else if (status == 'result') {
       status = 'live';
 
@@ -78,4 +102,12 @@ $(function() {
       $('canvas').css('display', 'none');
     }
   });
+
+
 });
+
+/*** HANDLE SWITCHING BETWEEN MODES ***/
+var mode = 'f1';
+var changeMode = function(nextMode) {
+  mode = nextMode;
+}
